ea0ac83f18989042669fbf8f084ac76fc95db628
Gromov Andrew <gromov7397@yandex.ru>
2026-01-24 21:41:10 +0300

    Добавил модул geocode_city.py

---
+++ functional_programming/3/geocode_city.py
@@ -0,0 +1,19 @@
+import requests
+import logging
+
+def get_geocode_city(city: str) -> dict|None:
+    end_point = 'https://geocoding-api.open-meteo.com/v1/search'
+    params = {
+        'name': city,
+        'count': 10,
+        'language': 'en',
+        'format': 'json',
+    }
+
+    response = requests.get(end_point, params=params)
+    response.raise_for_status()
+    logging.info('Ответ от сервера: ОК {}'.format(response.status_code))
+
+    data = response.json()
+    logging.debug(data)
+    return data
--- functional_programming/3/logconf.py
+++ functional_programming/3/logconf.py
@@ -2,8 +2,9 @@ import logging
 
 def setup_logging(path: str = 'weather.log') -> None:
     logging.basicConfig(
-        filename=path,
+        filename='weather.log',
+        filemode='w',
         level=logging.DEBUG,
-        format='%(asctime)s | %(levelname)s | %(message)s',
+        format='%(asctime)s | %(levelname)s | %(filename)s:%(lineno)d | %(funcName)s | %(message)s',
         encoding='utf-8'
     )
